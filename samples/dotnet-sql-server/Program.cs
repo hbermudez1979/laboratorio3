c3230104122f07b57a248931af8ed4f6c3f653e5
hbermudez1979 <74999368+hbermudez1979@users.noreply.github.com>
2021-01-19 21:03:18 -0600

    Update Program.cs

--- samples/dotnet-sql-server/Program.cs
+++ samples/dotnet-sql-server/Program.cs
@@ -67,7 +67,7 @@ namespace Dotnet_Backend
                     await context.Response.WriteAsJsonAsync<object[]>(
                         new object[]{
                         "Hello",
-                        "World","Heinner Bermudez Araya"
+                        "World","Heinner","Bermudez","Araya"
                     });
                 });
 
